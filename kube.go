d63828602a381682f7522b89c24a49aa5ad49de3
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-10-23 16:43:23 +0200

    adding init version of node reboot agent informer

---
+++ kube.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func k8sGetClientConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	return rest.InClusterConfig()
+}
+
+func k8sGetClient(kubeconfig string) (*kubernetes.Clientset, error) {
+	config, err := k8sGetClientConfig(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct the Kubernetes client
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
--- main.go
+++ main.go
@@ -4,18 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type Agent struct {
-	nodeName string
-}
-type Controller struct {
-	nodeName string
-}
+type Agent struct{}
+type Controller struct{}
 type FlagVars struct {
+	nodeName   string
+	kubeconfig string
 	subcommand string
 	agent      *Agent
 	controller *Controller
+	interval   time.Duration
 }
 
 func flags() *FlagVars {
@@ -24,23 +27,28 @@ func flags() *FlagVars {
 		controller: new(Controller),
 	}
 	agent := flag.NewFlagSet("agent", flag.ExitOnError)
-	agent.StringVar(&c.agent.nodeName, "node.name", "", "name of node")
+	agent.StringVar(&c.nodeName, "node.name", "", "name of node")
+	agent.StringVar(&c.kubeconfig, "kube.config", "", "kubernetes config")
+	agent.DurationVar(&c.interval, "update.interval", 10*time.Second, "time.Duration cache update intermal")
 
 	controller := flag.NewFlagSet("controller", flag.ExitOnError)
-	controller.StringVar(&c.controller.nodeName, "node.name", "", "name of node")
-	version := flag.NewFlagSet("version", flag.ExitOnError)
+	controller.StringVar(&c.nodeName, "node.name", "", "name of node")
+	controller.StringVar(&c.kubeconfig, "kube.config", "", "kubernetes config")
+	controller.DurationVar(&c.interval, "update.interval", 10*time.Second, "time.Duration cache update intermal")
 
-	// Verify that a subcommand has been provided
-	// os.Arg[0] is the main command
-	// os.Arg[1] will be the subcommand
-	if len(os.Args) < 2 {
-		msg := `k8s node updater.
+	version := flag.NewFlagSet("version", flag.ExitOnError)
+	HelpMsg := `k8s node updater.
 
 Commands:
   version       Get version
   agent         Run agent
   controller    Run controller`
-		fmt.Println(msg)
+
+	// Verify that a subcommand has been provided
+	// os.Arg[0] is the main command
+	// os.Arg[1] will be the subcommand
+	if len(os.Args) < 2 {
+		fmt.Println(HelpMsg)
 		os.Exit(1)
 	}
 
@@ -73,9 +81,27 @@ Commands:
 
 func main() {
 	config := flags()
+
+	client, err := k8sGetClient(config.kubeconfig)
+	if err != nil {
+		log.Error(fmt.Errorf("failed to get client: %v", err))
+	}
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
 	switch config.subcommand {
 	case "agent":
 		fmt.Println("agent subcommand")
+		selector := fmt.Sprintf("kubernetes.io/hostname=%v", config.nodeName)
+		agentController := newAgentNodeController(
+			client,
+			metav1.NamespaceAll,
+			config.interval,
+			metav1.ListOptions{LabelSelector: selector},
+		)
+		agentController.Run(stopCh)
+
 	case "controller":
 		fmt.Println("controller subcommand")
 	}
