80e9d897dcdf79fac1abe17439446b6b7c8f7e9d
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-10-30 14:02:04 +0100

    adding terminate of instance in terminate func

--- terminator.go
+++ terminator.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 )
@@ -54,10 +56,17 @@ func (t *Terminator) terminate(e *TerminatorEvent) error {
 	log.Infof("terminator get event %v", e.nodename)
 	err := setNodeUnschedulable(e.nodename, t.client)
 	if err != nil {
-		log.Errorf("failed to patch node %v", err.Error())
+		return fmt.Errorf("failed to patch node %v", err.Error())
 	}
 	// drain node
+
 	// terninate node
+	ec2Client := newEC2()
+	err = ec2Client.awsTerminateInstance(e.nodename)
+	if err != nil {
+		return fmt.Errorf("failed to terminate node %v %v", e.nodename, err.Error())
+	}
+
 	// wait for new node (sleep)
 	// release and go to next
 	return nil
