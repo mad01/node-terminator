fb02bfa7865056297acbdc176e9f863187b692d0
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-10-31 14:26:51 +0100

    adding some todos

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// TODO: implement to handle number of concurrent terminations
+// TODO: implement to take wait time.Duration before one node is considered done
+//		 and the worker proceds to next node
+
 func main() {
 	LogInit(false)
 	err := runCmd()
