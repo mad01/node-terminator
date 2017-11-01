f3ecfb834a67f21314f060c2fd857a6938e03093
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-11-01 13:55:57 +0100

    update terminator to have waitintercal, and newterminator to take kubeconfig for drains

--- terminator.go
+++ terminator.go
@@ -2,33 +2,40 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 )
 
-func newTerminatorEvent() *TerminatorEvent {
-	t := TerminatorEvent{}
+func newTerminatorEvent(nodename string) *TerminatorEvent {
+	t := TerminatorEvent{
+		nodename:     nodename,
+		waitInterval: 5 * time.Minute,
+	}
 	return &t
 }
 
 // TerminatorEvent event information needed to terminate a node
 type TerminatorEvent struct {
-	nodename string
+	nodename     string
+	waitInterval time.Duration
 }
 
-func newTerminator(client *kubernetes.Clientset) *Terminator {
+func newTerminator(client *kubernetes.Clientset, kubeconfig string) *Terminator {
 	t := Terminator{
-		events: make(chan TerminatorEvent),
-		client: client,
+		events:   make(chan TerminatorEvent),
+		client:   client,
+		eviction: newEviction(kubeconfig),
 	}
 	return &t
 }
 
 // Terminator handles node terminate events and handles the lifetime of the event
 type Terminator struct {
-	events chan TerminatorEvent
-	client *kubernetes.Clientset
+	events   chan TerminatorEvent
+	client   *kubernetes.Clientset
+	eviction *Eviction
 }
 
 // Run terminator
@@ -47,24 +54,29 @@ func (t *Terminator) Run(stopCh chan struct{}) {
 	}
 }
 
-func (t *Terminator) terminate(e *TerminatorEvent) error {
+func (t *Terminator) terminate(event *TerminatorEvent) error {
 	// TODO: implement drain node handling / eveicting of all pods on that node
 	// TODO: implement wait for graceperiod before doing force terminate of nodes
 
 	// set node ot no schedule
-	log.Infof("terminator get event %v", e.nodename)
-	err := setNodeUnschedulable(e.nodename, t.client)
+	log.Infof("terminator get event %v", event.nodename)
+	err := setNodeUnschedulable(event.nodename, t.client)
 	if err != nil {
 		return fmt.Errorf("failed to patch node %v", err.Error())
 	}
 	// drain node
 
+	err = t.eviction.DrainNode(event.nodename)
+	if err != nil {
+		log.Errorf("failed to drain node %v %v", event.nodename, err.Error())
+	}
+
 	// terninate node
 	// TODO: implement actuall node termination (only for worker nodes) master should be skipped
 	ec2Client := newEC2()
-	err = ec2Client.awsTerminateInstance(e.nodename)
+	err = ec2Client.awsTerminateInstance(event.nodename)
 	if err != nil {
-		return fmt.Errorf("failed to terminate node %v %v", e.nodename, err.Error())
+		return fmt.Errorf("failed to terminate node %v %v", event.nodename, err.Error())
 	}
 
 	// wait for new node (sleep)
