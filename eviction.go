b807d0d8652c0c7ef4e115fe2be77252f571f9f6
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-11-01 13:57:37 +0100

    change new eviction to not return err

--- eviction.go
+++ eviction.go
@@ -10,7 +10,6 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/kubernetes/pkg/kubectl/cmd"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
@@ -23,29 +22,23 @@ import (
 // statefulset at the same time. Further improvements needs to be made to protect from this as
 // well.
 
-func drainNode(nodename string, client *kubernetes.Clientset) error {
-	// look at eviction policy
-	// client.Core().Pods().Evict
-	return nil
-}
-
-func newEviction(kubeconfig string) (*Eviction, error) {
+func newEviction(kubeconfig string) *Eviction {
 	config, err := k8sGetClientConfig(kubeconfig)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get kube rest config: %v", err.Error())
+		panic(fmt.Sprintf("failed to get kube rest config: %v", err.Error()))
 	}
 
 	e := Eviction{
-		DrainInterval: 5 * time.Minute,
-		ClientConfig:  kutil.NewClientConfig(config, metav1.NamespaceAll),
+		waitInterval: 5 * time.Minute,
+		ClientConfig: kutil.NewClientConfig(config, metav1.NamespaceAll),
 	}
-	return &e, nil
+	return &e
 }
 
 // Eviction struct
 type Eviction struct {
-	DrainInterval time.Duration
-	ClientConfig  clientcmd.ClientConfig
+	waitInterval time.Duration
+	ClientConfig clientcmd.ClientConfig
 }
 
 // DrainNode drains a K8s node.
@@ -85,9 +78,9 @@ func (e *Eviction) DrainNode(nodename string) error {
 		return fmt.Errorf("error draining node: %v", err)
 	}
 
-	if e.DrainInterval > time.Second*0 {
-		log.Infof("Waiting for %s for pods to stabilize after draining.", e.DrainInterval)
-		time.Sleep(e.DrainInterval)
+	if e.waitInterval > time.Second*0 {
+		log.Infof("Waiting for %s for pods to stabilize after draining.", e.waitInterval)
+		time.Sleep(e.waitInterval)
 	}
 
 	return nil
