15d005a8ede55d2ce426d14a513fe80aba7c3b4d
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-11-03 14:59:58 +0100

    change wait from 5 to 1, add set to track currently managed nodes that is terminating

--- terminator.go
+++ terminator.go
@@ -5,13 +5,15 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
+
+	set "gopkg.in/fatih/set.v0"
 	"k8s.io/client-go/kubernetes"
 )
 
 func newTerminatorEvent(nodename string) *TerminatorEvent {
 	t := TerminatorEvent{
 		nodename:     nodename,
-		waitInterval: 5 * time.Minute,
+		waitInterval: 1 * time.Minute,
 	}
 	return &t
 }
@@ -29,22 +31,24 @@ func newTerminator(kubeconfig string) *Terminator {
 	}
 
 	t := Terminator{
-		events:   make(chan TerminatorEvent),
-		client:   client,
-		eviction: newEviction(kubeconfig),
+		events:             make(chan TerminatorEvent),
+		client:             client,
+		eviction:           newEviction(kubeconfig),
+		activeTerminations: set.New(),
 	}
 	return &t
 }
 
 // Terminator handles node terminate events and handles the lifetime of the event
 type Terminator struct {
-	events   chan TerminatorEvent
-	client   *kubernetes.Clientset
-	eviction *Eviction
+	events             chan TerminatorEvent
+	client             *kubernetes.Clientset
+	eviction           *Eviction
+	activeTerminations *set.Set
 }
 
 // Run terminator
-func (t *Terminator) Run(stopCh chan struct{}) {
+func (t *Terminator) Run() {
 	for {
 		select {
 		case event := <-t.events:
@@ -52,24 +56,21 @@ func (t *Terminator) Run(stopCh chan struct{}) {
 			if err != nil {
 				log.Errorf("failed to terminate node %v %v", event.nodename, err.Error())
 			}
-		case _ = <-stopCh:
-			log.Info("stopping updater runner")
-			return
 		}
 	}
 }
 
 func (t *Terminator) terminate(event *TerminatorEvent) error {
-	// TODO: implement wait for graceperiod before doing force terminate of nodes
-
 	// set node ot no schedule
 	log.Infof("terminator get event %v", event.nodename)
+	t.activeTerminations.Add(event.nodename)
 	err := setNodeUnschedulable(event.nodename, t.client)
 	if err != nil {
 		return fmt.Errorf("failed to patch node %v", err.Error())
 	}
-	// drain node
 
+	// drain node
+	log.Infof("starting drain of node %v", event.nodename)
 	err = t.eviction.DrainNode(event.nodename)
 	if err != nil {
 		log.Errorf("failed to drain node %v %v", event.nodename, err.Error())
@@ -77,6 +78,7 @@ func (t *Terminator) terminate(event *TerminatorEvent) error {
 
 	// terninate node
 	// TODO: implement safeguard to never terminate master node only worker
+	log.Infof("starting termination of ec2 instance with node name %v", event.nodename)
 	ec2Client := newEC2()
 	err = ec2Client.awsTerminateInstance(event.nodename)
 	if err != nil {
@@ -84,6 +86,12 @@ func (t *Terminator) terminate(event *TerminatorEvent) error {
 	}
 
 	// wait for new node (sleep)
+	log.Infof("waiting for %s node to terminate %v", event.waitInterval, event.nodename)
+	time.Sleep(event.waitInterval)
+
 	// release and go to next
+	t.activeTerminations.Remove(event.nodename)
+	log.Infof("done terminating node %v", event.nodename)
+
 	return nil
 }
